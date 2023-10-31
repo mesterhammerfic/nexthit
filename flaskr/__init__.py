55169d7434d823d85464ab0807d084843053df91
max <maximilian.esterhammer-fic@gamalon.com>
2023-10-31 15:09:41 -0500

    set up database

---
+++ flaskr/__init__.py
@@ -0,0 +1,29 @@
+from flask import Flask
+import os
+from typing import Mapping
+
+def create_app(test_config: Mapping=None):
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY="dev",
+        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+    )
+
+    from . import db
+    db.init_app(app)
+
+    if test_config is None:
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        app.config.from_mapping(test_config)
+
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    @app.route('/hello')
+    def hello():
+        return 'Hello, World!'
+
+    return app
