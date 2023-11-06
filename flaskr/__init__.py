96759dd5992a99d9eb64cb9aa975a99f0b6f1c1c
max <maximilian.esterhammer-fic@gamalon.com>
2023-11-06 13:04:48 -0600

    fix tests

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,36 +1,50 @@
-from flask import Flask
 import os
-from typing import Mapping
 
-def create_app(test_config: Mapping=None):
+from flask import Flask
+
+
+def create_app(test_config=None):
+    """Create and configure an instance of the Flask application."""
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
+        # a default secret that should be overridden by instance config
         SECRET_KEY="dev",
-        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+        # store the database in the instance folder
+        DATABASE=os.path.join(app.instance_path, "flaskr.sqlite"),
     )
 
-    from . import db
-    db.init_app(app)
-
     if test_config is None:
-        app.config.from_pyfile('config.py', silent=True)
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile("config.py", silent=True)
     else:
-        app.config.from_mapping(test_config)
+        # load the test config if passed in
+        app.config.update(test_config)
 
+    # ensure the instance folder exists
     try:
         os.makedirs(app.instance_path)
     except OSError:
         pass
 
-    @app.route('/hello')
+    @app.route("/hello")
     def hello():
-        return 'Hello, World!'
+        return "Hello, World!"
 
-    from . import auth
+    # register the database commands
+    from flaskr import db
+
+    db.init_app(app)
+
+    # apply the blueprints to the app
+    from flaskr import auth, blog
+
     app.register_blueprint(auth.bp)
-
-    from . import blog
     app.register_blueprint(blog.bp)
+
+    # make url_for('index') == url_for('blog.index')
+    # in another app, you might define a separate main index here with
+    # app.route, while giving the blog blueprint a url_prefix, but for
+    # the tutorial the blog will be the main index
     app.add_url_rule("/", endpoint="index")
 
     return app
--- flaskr/blog.py
+++ flaskr/blog.py
@@ -1,6 +1,10 @@
-from flask import (
-    Blueprint, flash, g, redirect, render_template, request, url_for
-)
+from flask import Blueprint
+from flask import flash
+from flask import g
+from flask import redirect
+from flask import render_template
+from flask import request
+from flask import url_for
 from werkzeug.exceptions import abort
 
 from flaskr.auth import login_required
@@ -11,48 +15,41 @@ bp = Blueprint("blog", __name__)
 
 @bp.route("/")
 def index():
+    """Show all the posts, most recent first."""
     db = get_db()
     posts = db.execute(
-        "SELECT p.id, title, body, created, author_id, username "
-        "FROM post p JOIN user u ON p.author_id = u.id "
-        "ORDER BY created DESC"
+        "SELECT p.id, title, body, created, author_id, username"
+        " FROM post p JOIN user u ON p.author_id = u.id"
+        " ORDER BY created DESC"
     ).fetchall()
     return render_template("blog/index.html", posts=posts)
 
-@bp.route("/create", methods=("GET", "POST"))
-@login_required
-def create():
-    if request.method == "POST":
-        error = None
-        title = request.form["title"]
-        body = request.form["body"]
-        if title is None:
-            error = "Title Required"
 
-        if error is not None:
-            flash(error)
-        else:
-            db = get_db()
-            db.execute(
-                "INSERT INTO post (title, body, author_id) "
-                "VALUES (?, ?, ?)",
-                (title, body, g.user["id"]),
-            )
-            db.commit()
-            return redirect(url_for("blog.index"))
-    return render_template("blog/create.html")
+def get_post(id, check_author=True):
+    """Get a post and its author by id.
 
+    Checks that the id exists and optionally that the current user is
+    the author.
 
-def get_post(id_, check_author=True):
-    post = get_db().execute(
-        'SELECT p.id, title, body, created, author_id, username '
-        'FROM post p JOIN user u ON p.author_id = u.id '
-        'WHERE p.id = ?',
-        (id_,)
-    ).fetchone()
+    :param id: id of post to get
+    :param check_author: require the current user to be the author
+    :return: the post with author information
+    :raise 404: if a post with the given id doesn't exist
+    :raise 403: if the current user isn't the author
+    """
+    post = (
+        get_db()
+        .execute(
+            "SELECT p.id, title, body, created, author_id, username"
+            " FROM post p JOIN user u ON p.author_id = u.id"
+            " WHERE p.id = ?",
+            (id,),
+        )
+        .fetchone()
+    )
 
     if post is None:
-        abort(404, f"Post id {id_} doesn't exist.")
+        abort(404, f"Post id {id} doesn't exist.")
 
     if check_author and post["author_id"] != g.user["id"]:
         abort(403)
@@ -60,10 +57,37 @@ def get_post(id_, check_author=True):
     return post
 
 
-@bp.route('/<int:id_>/update', methods=("GET", "POST"))
+@bp.route("/create", methods=("GET", "POST"))
 @login_required
-def update(id_):
-    post = get_post(id_)
+def create():
+    """Create a new post for the current user."""
+    if request.method == "POST":
+        title = request.form["title"]
+        body = request.form["body"]
+        error = None
+
+        if not title:
+            error = "Title is required."
+
+        if error is not None:
+            flash(error)
+        else:
+            db = get_db()
+            db.execute(
+                "INSERT INTO post (title, body, author_id) VALUES (?, ?, ?)",
+                (title, body, g.user["id"]),
+            )
+            db.commit()
+            return redirect(url_for("blog.index"))
+
+    return render_template("blog/create.html")
+
+
+@bp.route("/<int:id>/update", methods=("GET", "POST"))
+@login_required
+def update(id):
+    """Update a post if the current user is the author."""
+    post = get_post(id)
 
     if request.method == "POST":
         title = request.form["title"]
@@ -71,29 +95,31 @@ def update(id_):
         error = None
 
         if not title:
-            error = "Title Required"
+            error = "Title is required."
 
         if error is not None:
             flash(error)
         else:
             db = get_db()
             db.execute(
-                'UPDATE post SET title = ?, body = ?'
-                'WHERE id = ?',
-                (title, body, id_)
+                "UPDATE post SET title = ?, body = ? WHERE id = ?", (title, body, id)
             )
             db.commit()
             return redirect(url_for("blog.index"))
+
     return render_template("blog/update.html", post=post)
 
 
-@bp.route("/<int:id_>/delete")
+@bp.route("/<int:id>/delete", methods=("POST",))
 @login_required
-def delete(id_):
+def delete(id):
+    """Delete a post.
+
+    Ensures that the post exists and that the logged in user is the
+    author of the post.
+    """
+    get_post(id)
     db = get_db()
-    db.execute(
-        "DELETE FROM post WHERE id = ?",
-        (id_,)
-    )
+    db.execute("DELETE FROM post WHERE id = ?", (id,))
     db.commit()
     return redirect(url_for("blog.index"))
